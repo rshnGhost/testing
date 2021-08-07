cf1140919e7b2837d4d3d5dc0dfb9c72a8b3acab
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-07 23:56:05 +0530

    all try catch

--- testScript.ps1
+++ testScript.ps1
@@ -2,41 +2,55 @@ $pythonVersion = '3.9'
 
 ## checking Chocolatey
 Write-Host -NoNewline "checking chocolatey..."
-
 Try{
 	# Check if winget is already installed
 	$er = (invoke-expression "choco -v") 2>&1
 	if ($lastexitcode) {throw $er}
-	Write-Host "choco is already installed."
+	Write-Output "[Found]"
 }
 Catch{
-	# winget is not installed. Install it from the Github release
-	Write-Host "choco is not found, installing it right now."
-}
-Finally {
-  Write-Host "final Block"
+	Write-Output "[Not Found]"
+	Write-Host "installing chocolatey..."
+	## installing Chocolatey
+	# Set directory for installation - Chocolatey does not lock
+	# down the directory if not the default
+	$InstallDir='C:\ProgramData\chocolatey'
+	## $env:ChocolateyInstall="$InstallDir"
+
+	# If your PowerShell Execution policy is restrictive, you may
+	# not be able to get around that. Try setting your session to
+	# Bypass.
+	Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+	# All install options - offline, proxy, etc at
+	# https://chocolatey.org/install
+	iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
 ## checking python
 Write-Host -NoNewline "checking python..."
-$testpython = python -V
-if($testpython -eq ""){
+Try{
+	# Check if python is already installed
+	$er = (invoke-expression "python -V") 2>&1
+	if ($lastexitcode) {throw $er}
+	Write-Output "[Found]"
+}
+Catch{
 	Write-Output "[Not Found]"
 	Write-Host "installing python..."
-	## installing python
 	choco install python --version=$pythonVersion -y
-}else{
-	Write-Output "[Found]"
 }
 
 ## checking pipenv
 Write-Host -NoNewline "checking pipenv..."
-$testpipenv = python -m pipenv --version
-if($testpipenv -eq ""){
+Try{
+	# Check if pipenv is already installed
+	$er = (invoke-expression "python -m pipenv --version") 2>&1
+	if ($lastexitcode) {throw $er}
+	Write-Output "[Found]"
+}
+Catch{
 	Write-Output "[Not Found]"
 	Write-Host "installing pipenv..."
-	## install pipenv module
 	python -m pip install pipenv
-}else{
-	Write-Output "[Found]"
 }
