5703512e999f8714aa437219b576a7895eb078e8
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-08 20:51:46 +0530

    added: download python installing after checking

--- downloadPython.ps1
+++ downloadPython.ps1
@@ -6,11 +6,14 @@ $download = "https://github.com/rshnGhost/"+$pName+"/archive/refs/heads/"+$fName
 $output = "C:\Temp\python-3.9.6-amd64.exe"
 
 $statusFile = Test-Path C:\Temp\python-3.9.6-amd64.exe -PathType Leaf
+Write-Host -NoNewline "Checking latest release"
 If (!$statusFile) {
-  Write-Host "Dowloading latest release"
+  Write-Host "[File not found]"
+  Write-Host -NoNewline "Dowloading latest release"
   Invoke-WebRequest -Uri $url -OutFile $output
+  Write-Host "[Downloaded]"
 }
 else{
-  Write-Host "File found"
+  Write-Host "[File found]"
   & C:\Temp\python-3.9.6-amd64.exe /passive /install
 }
