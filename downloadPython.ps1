cb56135a6e17a0b4069d549dc72dbaa3ef4e242a
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-08 20:38:57 +0530

    added: download python

--- downloadPython.ps1
+++ downloadPython.ps1
@@ -1,6 +1,15 @@
+## iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/JR4jF'))
+
 $pythonVersion = '3.9.6'
 $url = "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe"
 $download = "https://github.com/rshnGhost/"+$pName+"/archive/refs/heads/"+$fName+".zip"
 $output = "C:\Temp\python-3.9.6-amd64.exe"
-Write-Host "Dowloading latest release"
-Invoke-WebRequest -Uri $url -OutFile $output
+
+$statusFile = Test-Path C:\Temp\python-3.9.6-amd64.exe -PathType Leaf
+If (!$statusFile) {
+  Write-Host "Dowloading latest release"
+  Invoke-WebRequest -Uri $url -OutFile $output
+}
+else{
+  Write-Host "File found"
+}
