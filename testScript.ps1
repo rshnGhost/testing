b70596edac837046da4cacb87719c2ed728bb15d
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-09 15:16:09 +0530

    clean code

--- testScript.ps1
+++ testScript.ps1
@@ -1,81 +1,86 @@
-function installChocolatey {
-	try{
-		Write-Host -NoNewline "checking chocolatey..."
-		$er = (invoke-expression "choco -v") 2>&1
-		if ($lastexitcode) {throw $er}
-		if (!$lastexitcode) {Write-Output "[Found]"}
-	}
-	catch{
+$python = 0
+$pipenv = 0
+$choco = 0
+
+function checkChocolatey {
+	Write-Host -NoNewline "Checking Chocolatey..."
+	$er = (invoke-expression "choco -v") 2>&1
+	if ($lastexitcode) {
 		Write-Output "[Not Found]"
-		$InstallDir='C:\ProgramData\chocolatey'
-		$env:ChocolateyInstall="$InstallDir"
-		Set-ExecutionPolicy Bypass -Scope Process -Force;
-		iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+		$choco = 0
+	}
+	if (!$lastexitcode) {
+		Write-Output "[Found]"
+		$choco = 1
+	}
+}
+
+function checkPython {
+	Write-Host -NoNewline "Checking Python..."
+	$er = (invoke-expression "python -V") 2>&1
+	if ($lastexitcode) {
+		Write-Output "[Not Found]"
+		$python = 0
+	}
+	if (!$lastexitcode) {
+		Write-Output "[Found]"
+		$python = 1
+	}
+}
+
+function checkPipenv {
+	Write-Host -NoNewline "Checking Pipenv..."
+	$er = (invoke-expression "python -m pipenv --version") 2>&1
+	if ($lastexitcode) {
+		Write-Output "[Not Found]"
+		$pipenv = 0
+	}
+	if (!$lastexitcode) {
+		Write-Output "[Found]"
+		$pipenv = 1
+	}
+}
+
+function installChocolatey {
+	Write-Host -NoNewline "Installing Chocolatey..."
+	$InstallDir='C:\ProgramData\chocolatey'
+	$env:ChocolateyInstall="$InstallDir"
+	Set-ExecutionPolicy Bypass -Scope Process -Force;
+	iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+	$er = (invoke-expression "choco -v") 2>&1
+	if ($lastexitcode) {
+		Write-Output "[Not Installed]"
+		$choco = 0
+	}
+	if (!$lastexitcode) {
+		Write-Output "[Installed]"
+		$choco = 1
 	}
 }
 
 function installPython {
-	try{
-		Write-Host -NoNewline "checking python..."
-		$er = (invoke-expression "python -V") 2>&1
-		if ($lastexitcode) {throw $er}
-		if (!$lastexitcode) {Write-Output "[Found]"}
+	Write-Host -NoNewline "Installing Python..."
+	$er = (invoke-expression "choco install python --version=3.9.6 -y") 2>&1
+	if ($lastexitcode) {
+		Write-Output "[Not Installed]"
+		$python = 0
 	}
-	catch{
-		Write-Output "[Not Found]"
-	}
-}
-
-function Pipenv {
-	try{
-		Write-Host -NoNewline "checking pipenv..."
-		$er = (invoke-expression "python -m pipenv --version") 2>&1
-		if ($lastexitcode) {throw $er}
-	}
-	catch{
-		Write-Output "[Not Found]"
+	if (!$lastexitcode) {
+		Write-Output "[Installed]"
+		$python = 1
 	}
 }
 
-Try{
-	# Check if pipenv is already installed
-	Write-Host -NoNewline "checking pipenv..."
-	$er = (invoke-expression "python -m pipenv --version") 2>&1
-	if ($lastexitcode) {throw $er}
-	if (!$lastexitcode) {Write-Output "[Found]"}
+function installPipenv {
+	Write-Host -NoNewline "Installing Pipenv..."
+	$er = (invoke-expression "python -m pip install pipenv") 2>&1
+	if ($lastexitcode) {
+		Write-Output "[Not Installed]"
+		$pipenv = 0
+	}
+	if (!$lastexitcode) {
+		Write-Output "[Installed]"
+		$pipenv = 1
+	}
 }
-Catch{
-	Write-Output "[Not Found]"
-	## checking python
-	Try{
-		# Check if python is already installed
-		Write-Host -NoNewline "checking python..."
-		$er = (invoke-expression "python -V") 2>&1
-		if ($lastexitcode) {throw $er}
-		if (!$lastexitcode) {
-			Write-Output "[Found]"
-			Write-Host -NoNewline "installing pipenv..."
-			python -m pip install pipenv
-			Write-Output "[Done]"
-		}
-	}
-	Catch{
-		Write-Output "[Not Found]"
-		Try{
-				Write-Host -NoNewline "checking python..."
-				$er = (invoke-expression "choco -v") 2>&1
-				if ($lastexitcode) {throw $er}
-				if (!$lastexitcode) {
-					Write-Host "[Done]"
-					Write-Host -NoNewline "checking pipenv..."
-					python -m pip install pipenv
-					Write-Host "[Done]"
-				}
-			}
-			Catch{
-				Write-Output "[Failed]"
-			}
 
-	}
-			
-		}
