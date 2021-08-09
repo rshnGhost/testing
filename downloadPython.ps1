de8dcd6ab091991c2023ab80e04b0147f5ffc81e
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-09 10:44:31 +0530

    testing: full install

--- downloadPython.ps1
+++ downloadPython.ps1
@@ -5,27 +5,56 @@ $url = "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe"
 $download = "https://github.com/rshnGhost/"+$pName+"/archive/refs/heads/"+$fName+".zip"
 $output = "C:\Temp\python-3.9.6-amd64.exe"
 
-$statusFile = Test-Path C:\Temp\python-3.9.6-amd64.exe -PathType Leaf
-Write-Host -NoNewline "Checking latest release"
-If (!$statusFile) {
-  Write-Host "[File not found]"
-  Write-Host -NoNewline "Dowloading latest release"
-  Invoke-WebRequest -Uri $url -OutFile $output
-  Write-Host "[Downloaded]"
+Try{
+	# Check if pipenv is already installed
+	$er = (invoke-expression "python -m pipenv --version") 2>&1
+	if ($lastexitcode) {throw $er}
+	Write-Output "[Found]"
+	$pip = 1
 }
-else{
-  Write-Host "[File found]"
-  Write-Host -NoNewline "Installing latest release"
-	$exe = 'C:\Temp\python-3.9.6-amd64.exe'
-	$args = '/passive', '/install', 'InstallAllUsers=1'
-	Start-Process -Wait $exe -ArgumentList $args
-  pause
-  Try{
+Catch{
+	Write-Output "[Not Found]"
+	$pip = 0
+	## checking python
+	Write-Host -NoNewline "checking python..."
+	Try{
+		# Check if python is already installed
 		$er = (invoke-expression "python -V") 2>&1
 		if ($lastexitcode) {throw $er}
-		if (!$lastexitcode) {Write-Host "[Done]"}
+		Write-Output "[Found]"
+		$python = 1
+		Write-Host -NoNewline "installing pipenv..."
+		python -m pip install pipenv
+		Write-Output "[Done]"
 	}
 	Catch{
-		Write-Output "[Failed]"
+		Write-Output "[Not Found]"
+		$python = 0
+		$statusFile = Test-Path C:\Temp\python-3.9.6-amd64.exe -PathType Leaf
+		Write-Host -NoNewline "Checking latest release"
+		If (!$statusFile) {
+			Write-Host "[File not found]"
+			Write-Host -NoNewline "Dowloading latest release"
+			Invoke-WebRequest -Uri $url -OutFile $output
+			Write-Host "[Downloaded]"
+		}
+		else
+		{
+			Write-Host "[File found]"
+			Write-Host -NoNewline "Installing latest release"
+			$exe = 'C:\Temp\python-3.9.6-amd64.exe'
+			$args = '/passive', '/install', 'InstallAllUsers=1'
+			Start-Process -Wait $exe -ArgumentList $args
+			Try{
+				$er = (invoke-expression "python -V") 2>&1
+				if ($lastexitcode) {throw $er}
+				if (!$lastexitcode) {
+					Write-Host "[Done]"
+					python -m pip install pipenv
+				}
+				
+			}
+			Catch{
+				Write-Output "[Failed]"
+			}
 	}
-}
