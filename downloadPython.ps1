4ba50b75412650ff9e40a3a432185c001ee8a1c6
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-09 09:23:53 +0530

    edited: install option

--- downloadPython.ps1
+++ downloadPython.ps1
@@ -16,7 +16,7 @@ If (!$statusFile) {
 else{
   Write-Host "[File found]"
   Write-Host -NoNewline "Installing latest release"
-  & C:\Temp\python-3.9.6-amd64.exe /quiet InstallAllUsers=1
+  & C:\Temp\python-3.9.6-amd64.exe /passive /install InstallAllUsers=1
   pause
   Try{
 		$er = (invoke-expression "python -V") 2>&1
