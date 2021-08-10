dc86f7ee4741906f512c00a0e3e5fea6487fd5e6
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-10 11:04:04 +0530

    removed: sub process

--- testScript.ps1
+++ testScript.ps1
@@ -5,128 +5,116 @@ $pipenv = 0
 $choco = 0
 
 function checkChocolatey {
-	Start-Process -Wait powershell {
-		try{
-			Write-Host -NoNewline "Checking Chocolatey..."
-			$er = (invoke-expression "choco -v") 2>&1
-			if ($lastexitcode) {throw $er}
-			if (!$lastexitcode) {
-				Write-Host "[Found]"
-				$choco = 1
-				return 1
-			}
+	try{
+		Write-Host -NoNewline "Checking Chocolatey..."
+		$er = (invoke-expression "choco -v") 2>&1
+		if ($lastexitcode) {throw $er}
+		if (!$lastexitcode) {
+			Write-Host "[Found]"
+			$choco = 1
+			return 1
 		}
-		catch{
-			Write-Host "[Not Found]"
-			$choco = 0
-			return 0
-		}
+	}
+	catch{
+		Write-Host "[Not Found]"
+		$choco = 0
+		return 0
 	}
 }
 
 function checkPython {
-	Start-Process -Wait powershell {
-		try{
-			Write-Host -NoNewline "Checking Python..."
-			$er = (invoke-expression "python -V") 2>&1
-			if ($lastexitcode) {throw $er}
-			if (!$lastexitcode) {
-				Write-Host "[Found]"
-				$python = 1
-				return 1
-			}
+	try{
+		Write-Host -NoNewline "Checking Python..."
+		$er = (invoke-expression "python -V") 2>&1
+		if ($lastexitcode) {throw $er}
+		if (!$lastexitcode) {
+			Write-Host "[Found]"
+			$python = 1
+			return 1
 		}
-		catch{
-			Write-Host "[Not Found]"
-			$python = 0
-			return 0
-		}
+	}
+	catch{
+		Write-Host "[Not Found]"
+		$python = 0
+		return 0
 	}
 }
 
 function checkPipenv {
-	Start-Process -Wait powershell {
-		try{
-			Write-Host -NoNewline "Checking Pipenv..."
-			$er = (invoke-expression "python -m pipenv --version") 2>&1
-			if ($lastexitcode) {throw $er}
-			if (!$lastexitcode) {
-				Write-Host "[Found]"
-				$pipenv = 1
-				return 1
-			}
+	try{
+		Write-Host -NoNewline "Checking Pipenv..."
+		$er = (invoke-expression "python -m pipenv --version") 2>&1
+		if ($lastexitcode) {throw $er}
+		if (!$lastexitcode) {
+			Write-Host "[Found]"
+			$pipenv = 1
+			return 1
 		}
-		catch{
-			Write-Host "[Not Found]"
-			$pipenv = 0
-			return 0
-		}
+	}
+	catch{
+		Write-Host "[Not Found]"
+		$pipenv = 0
+		return 0
 	}
 }
 
 function installChocolatey {
-	Start-Process -Wait powershell {
-		try{
-			Write-Host -NoNewline "Installing Chocolatey..."
-			$InstallDir='C:\ProgramData\chocolatey'
-			$env:ChocolateyInstall="$InstallDir"
-			Set-ExecutionPolicy Bypass -Scope Process -Force;
-			iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-			$er = (invoke-expression "choco -v") 2>&1
-			if ($lastexitcode) {throw $er}
-			if (!$lastexitcode) {
-				Write-Host "[Installed]"
-				$choco = 1
-				return 1
-			}
+	try{
+		Write-Host -NoNewline "Installing Chocolatey..."
+		$InstallDir='C:\ProgramData\chocolatey'
+		$env:ChocolateyInstall="$InstallDir"
+		Set-ExecutionPolicy Bypass -Scope Process -Force;
+		iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+		$er = (invoke-expression "choco -v") 2>&1
+		if ($lastexitcode) {throw $er}
+		if (!$lastexitcode) {
+			Write-Host "[Installed]"
+			$choco = 1
+			return 1
 		}
-		catch{
-			Write-Host "[Not Installed]"
-			$choco = 0
-			return 0
-		}
+	}
+	catch{
+		Write-Host "[Not Installed]"
+		$choco = 0
+		return 0
 	}
 }
 
 function installPython {
-	Start-Process -Wait powershell {
-		try{
-			Write-Host -NoNewline "Installing Python..."
-			$er = (invoke-expression "choco install python --version=3.9.6 -y") 2>&1
-			if ($lastexitcode) {throw $er}
-			checkPython
-			if (!$lastexitcode) {
-				Write-Host "[Installed]"
-				refreshenv
-				$python = 1
-				return 1
-			}
+	try{
+		Write-Host -NoNewline "Installing Python..."
+		$er = (invoke-expression "choco install python --version=3.9.6 -y") 2>&1
+		if ($lastexitcode) {throw $er}
+		checkPython
+		if (!$lastexitcode) {
+			Write-Host "[Installed]"
+			refreshenv
+			$python = 1
+			return 1
 		}
-		catch{
-			Write-Host "[Not Installed]"
-			$python = 0
-			return 0
-		}
+	}
+	catch{
+		Write-Host "[Not Installed]"
+		$python = 0
+		return 0
 	}
 }
 
 function installPipenv {
-	Start-Process -Wait powershell {
-		try{
-			Write-Host -NoNewline "Installing Pipenv..."
-			$er = (invoke-expression "python -m pip install pipenv") 2>&1
-			if ($lastexitcode) {throw $er}
-			if (!$lastexitcode) {
-				Write-Host "[Installed]"
-				$pipenv = 1
-				return 1
-			}
+	try{
+		Write-Host -NoNewline "Installing Pipenv..."
+		$er = (invoke-expression "python -m pip install pipenv") 2>&1
+		if ($lastexitcode) {throw $er}
+		if (!$lastexitcode) {
+			Write-Host "[Installed]"
+			$pipenv = 1
+			return 1
 		}
-		catch{
-			Write-Host "[Not Installed]"
-			$pipenv = 0
-			return 0
-		}
+	}
+	catch{
+		Write-Host "[Not Installed]"
+		$pipenv = 0
+		return 0
 	}
 }
 
