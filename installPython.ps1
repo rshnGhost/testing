06a9c7062492ba07a753b113a04578eb682abd39
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-11 16:00:34 +0530

    edited: fetch of sha1

--- installPython.ps1
+++ installPython.ps1
@@ -1,4 +1,4 @@
-$er = (invoke-expression "$webData = Invoke-WebRequest -Uri 'https://api.github.com/repos/rshnGhost/testing/commits'") 2>&1
-$er = (invoke-expression "$releases = ConvertFrom-Json $webData.content") 2>&1
-$er = (invoke-expression "$releases | get-member") 2>&1
+$webData = Invoke-WebRequest -Uri 'https://api.github.com/repos/rshnGhost/testing/commits'
+$releases = ConvertFrom-Json $webData.content
+$releases | get-member
 Write-Host $releases.sha[0]
