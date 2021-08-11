d4ae420596da07f56f504ce1f352b6300274b650
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-11 10:23:32 +0530

    added: tab for message begin

--- downloadPython.ps1
+++ downloadPython.ps1
@@ -3,55 +3,55 @@
 $pythonVersion = '3.9.6'
 
 # Check if operating system architecture
-	Write-Host -NoNewline "checking architecture..."
+	Write-Host -NoNewline "checking architecture...`t"
 if (($env:PROCESSOR_ARCHITECTURE -eq "AMD64") -and ([Environment]::Is64BitOperatingSystem)) {
-	Write-Host "`t[64bit]"
+	Write-Host "[64bit]"
 	$url = "https://www.python.org/ftp/python/"+$pythonVersion+"/python-"+$pythonVersion+"-amd64.exe"
 	$output = "C:\Temp\python-"+$pythonVersion+"-amd64.exe"
 }
 else{
-	Write-Host "`t[32bit]"
+	Write-Host "[32bit]"
 	$url = "https://www.python.org/ftp/python/"+$pythonVersion+"/python-"+$pythonVersion+".exe"
 	$output = "C:\Temp\python-"+$pythonVersion+".exe"
 }
 
 Try{
 	# Check if pipenv is already installed
-	Write-Host -NoNewline "checking pipenv..."
+	Write-Host -NoNewline "checking pipenv...`t`t"
 	$er = (invoke-expression "python -m pipenv --version") 2>&1
 	if ($lastexitcode) {throw $er}
-	Write-Host "`t`t[Found]"
+	Write-Host "[Found]"
 	$pip = 1
 }
 Catch{
-	Write-Host "`t`t[Not Found]"
+	Write-Host "[Not Found]"
 	$pip = 0
 	## checking python
-	Write-Host -NoNewline "checking python..."
+	Write-Host -NoNewline "checking python...`t`t"
 	Try{
 		# Check if python is already installed
 		$er = (invoke-expression "python -V") 2>&1
 		if ($lastexitcode) {throw $er}
-		Write-Host "`t[Found]"
+		Write-Host "[Found]"
 		$python = 1
-		Write-Host -NoNewline "installing pipenv..."
+		Write-Host -NoNewline "installing pipenv...`t`t"
 		python -m pip install pipenv
-		Write-Host "`t[Done]"
+		Write-Host "[Done]"
 	}
 	Catch{
-		Write-Host "`t[Not Found]"
+		Write-Host "[Not Found]"
 		$python = 0
 		$statusFile = Test-Path $output -PathType Leaf
-		Write-Host -NoNewline "Checking latest release"
+		Write-Host -NoNewline "Checking latest release`t`t"
 		If (!$statusFile){
-			Write-Host "`t[File not found]"
-			Write-Host -NoNewline "Dowloading latest release"
+			Write-Host "[File not found]"
+			Write-Host -NoNewline "Dowloading latest release`t`t"
 			Invoke-WebRequest -Uri $url -OutFile $output
-			Write-Host "`t[Downloaded]"
+			Write-Host "[Downloaded]"
 		}
 		else{
-			Write-Host "`t[File found]"
-			Write-Host -NoNewline "Installing latest release"
+			Write-Host "[File found]"
+			Write-Host -NoNewline "Installing latest release`t`t"
 			$exe = 'C:\Temp\python-3.9.6-amd64.exe'
 			$args = '/passive', 'install', 'InstallAllUsers=1', 'PrependPath=1', 'Include_test=0'
 			Start-Process -Wait $exe -ArgumentList $args
@@ -64,18 +64,18 @@ Catch{
 		}
 		start powershell{
 			Try{
-				Write-Host -NoNewline "checking python..."
+				Write-Host -NoNewline "checking python...`t`t"
 				$er = (invoke-expression "python -V") 2>&1
 				if ($lastexitcode) {throw $er}
 				if (!$lastexitcode) {
-					Write-Host "`t[Done]"
-					Write-Host -NoNewline "checking pipenv..."
+					Write-Host "[Done]"
+					Write-Host -NoNewline "checking pipenv...`t`t"
 					python -m pip install pipenv
-					Write-Host "`t[Done]"
+					Write-Host "[Done]"
 				}
 			}
 			Catch{
-				Write-Host "`t[Failed]"
+				Write-Host "[Failed]"
 			}
 		}
 	}
