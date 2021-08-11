fff9ec2f9348cf78aa7720a8ed4ef43b011fa0c9
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-11 23:50:26 +0530

    checking code

--- webInstall.ps1
+++ webInstall.ps1
@@ -1,12 +1,14 @@
 $url = "https://api.github.com/repos/rshnGhost/django-quick/commits"
 $webData = Invoke-WebRequest -Uri $url -UseBasicParsing
 $releases = ConvertFrom-Json $webData.content
-Write-Host $releases.sha[0].substring(0, [System.Math]::Min(7, $s.Length))
+Write-Host $releases.sha[0].substring(0, [System.Math]::Min(7, $releases.Length))
 
 $fName = 'django-3.2.5'
-$dName = 'django-quick['+$releases.sha[0].substring(0, [System.Math]::Min(7, $s.Length))+'].zip'
 $pName = 'django-quick'
+$dName = $pName+'-'+$releases.sha[0].substring(0, [System.Math]::Min(7, $releases.Length))+'.zip'
 $output = "C:\Temp\$dName"
+$download = "https://github.com/rshnGhost/"+$pName+"/archive/refs/heads/"+$fName+".zip"
+
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
@@ -19,13 +21,26 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 	Exit
 }
 
+Write-Host -NoNewline "Checking for file`t"
+$statusFile = Test-Path "C:\Temp\$dName" -PathType Leaf
+if (!$statusFile) {
+	Write-Host "[File not Found]"
+	Write-Host -NoNewline "Dowloading latest release`t"
+	Invoke-WebRequest -Uri $download -OutFile $output
+}
+if ($statusFile) {
+	Write-Host "[File Found]"
+}
+
+
+<#
 # GUI Specs
 Write-Host "Checking for file..."
 cd 'C:\Temp\'
 $statusFile = Test-Path C:\Temp\$dName.zip -PathType Leaf
 If (!$statusFile) {
   Try{
-    $download = "https://github.com/rshnGhost/"+$pName+"/archive/refs/heads/"+$fName+".zip"
+
     Write-Host "Dowloading latest release"
     Invoke-WebRequest -Uri $download -OutFile $output
     Write-Output "Path of the file : $output"
@@ -61,3 +76,4 @@ If (!$statusFile) {
   Write-Host "Running..."
   & "C:\Temp\$pName-$dName\windowCmd\3 run.bat"
 }
+#>
