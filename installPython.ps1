8a2b592249fb5a9c5f1e33a6e94c1dc21655947c
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-11 15:55:00 +0530

    edited: fetch of sha

--- installPython.ps1
+++ installPython.ps1
@@ -1,4 +1,4 @@
 $er = (invoke-expression "$webData = Invoke-WebRequest -Uri 'https://api.github.com/repos/rshnGhost/testing/commits'") 2>&1
 $er = (invoke-expression "$releases = ConvertFrom-Json $webData.content") 2>&1
 $er = (invoke-expression "$releases | get-member") 2>&1
-$releases.sha[0]
+Write-Host $releases.sha[0]
