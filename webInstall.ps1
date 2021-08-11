c2404e6f92d3c24e8d991111ee0fb4792afe9f05
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-11 22:53:10 +0530

    fixing: file name for downloading

--- webInstall.ps1
+++ webInstall.ps1
@@ -3,9 +3,10 @@ $webData = Invoke-WebRequest -Uri $url -UseBasicParsing
 $releases = ConvertFrom-Json $webData.content
 Write-Host $releases.sha[0]
 
-$fName = 'django-3.2.5['+$releases.sha[0]+']'
+$fName = 'django-3.2.5'
+$dName = 'django-3.2.5['+$releases.sha[0]+']'
 $pName = 'django-quick'
-$output = "C:\Temp\$pName-$fName.zip"
+$output = "C:\Temp\$pName-$dName.zip"
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
@@ -21,42 +22,42 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 # GUI Specs
 Write-Host "Checking for file..."
 cd 'C:\Temp\'
-$statusFile = Test-Path C:\Temp\$pName-$fName.zip -PathType Leaf
+$statusFile = Test-Path C:\Temp\$pName-$dName.zip -PathType Leaf
 If (!$statusFile) {
   Try{
     $download = "https://github.com/rshnGhost/"+$pName+"/archive/refs/heads/"+$fName+".zip"
     Write-Host "Dowloading latest release"
     Invoke-WebRequest -Uri $download -OutFile $output
     Write-Output "Path of the file : $output"
-		$statusFolder = Test-Path C:\Temp\$pName-$fName
+		$statusFolder = Test-Path C:\Temp\$pName-$dName
 		If ($statusFolder) {
     	Write-Host "Deleting..."
-    	Remove-Item C:\Temp\$pName-$fName -Recurse
+    	Remove-Item C:\Temp\$pName-$dName -Recurse
 		}
     Write-Host "Expand Archive..."
     Expand-Archive $output 'C:\Temp\'
     Write-Host "Executing..."
-    cd "C:\Temp\$pName-$fName\windowCmd\"
+    cd "C:\Temp\$pName-$dName\windowCmd\"
     Write-Host "Setting up..."
-    & "C:\Temp\$pName-$fName\windowCmd\2 setup.bat"
+    & "C:\Temp\$pName-$dName\windowCmd\2 setup.bat"
     Write-Host "Running..."
-    & "C:\Temp\$pName-$fName\windowCmd\3 run.bat"
+    & "C:\Temp\$pName-$dName\windowCmd\3 run.bat"
   }
   Catch{
     Write-Host "Someting is not working"
   }
 } else {
-	$statusFolder = Test-Path C:\Temp\$pName-$fName
+	$statusFolder = Test-Path C:\Temp\$pName-$dName
 	If ($statusFolder) {
 		Write-Host "Deleting..."
-		Remove-Item C:\Temp\$pName-$fName -Recurse
+		Remove-Item C:\Temp\$pName-$dName -Recurse
 	}
   Write-Host "Expand Archive..."
   Expand-Archive $output 'C:\Temp\'
   Write-Host "Executing..."
-  cd "C:\Temp\$pName-$fName\windowCmd\"
+  cd "C:\Temp\$pName-$dName\windowCmd\"
   Write-Host "Setting up..."
-  & "C:\Temp\$pName-$fName\windowCmd\2 setup.bat"
+  & "C:\Temp\$pName-$dName\windowCmd\2 setup.bat"
   Write-Host "Running..."
-  & "C:\Temp\$pName-$fName\windowCmd\3 run.bat"
+  & "C:\Temp\$pName-$dName\windowCmd\3 run.bat"
 }
