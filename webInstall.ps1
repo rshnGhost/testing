7b5a0ccfbbf875b9f30c5981f5a36afc49122489
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-11 22:55:23 +0530

    fixing: api url

--- webInstall.ps1
+++ webInstall.ps1
@@ -1,4 +1,4 @@
-$url = "https://api.github.com/repos/rshnGhost/testing/commits"
+$url = "https://api.github.com/repos/rshnGhost/django-quick/commits"
 $webData = Invoke-WebRequest -Uri $url -UseBasicParsing
 $releases = ConvertFrom-Json $webData.content
 Write-Host $releases.sha[0]
