9e93f6385f6e9b0886f54f886dddc9e9f64f5025
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-11 23:06:32 +0530

    reducing: sha value to first 7

--- installPython.ps1
+++ installPython.ps1
@@ -1,4 +1,4 @@
 $url = "https://api.github.com/repos/rshnGhost/testing/commits"
 $webData = Invoke-WebRequest -Uri $url -UseBasicParsing
 $releases = ConvertFrom-Json $webData.content
-Write-Host $releases.sha[0][0-6]
+Write-Host $releases.sha[0].substring(0,6)
