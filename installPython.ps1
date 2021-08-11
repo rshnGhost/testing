a3830867bf8c17b965de9e9f4beadcbdc887e172
myS3lfGhost <31742263+rshnGhost@users.noreply.github.com>
2021-08-11 22:44:27 +0530

    added: UseBasicParsing

--- installPython.ps1
+++ installPython.ps1
@@ -1,4 +1,4 @@
 $url = "https://api.github.com/repos/rshnGhost/testing/commits"
-$webData = Invoke-WebRequest -Uri $url
+$webData = Invoke-WebRequest -Uri $url -UseBasicParsing
 $releases = ConvertFrom-Json $webData.content
 Write-Host $releases.sha[0]
