db92e7da02728758a05b0ec5188a6642dfe37326
rshnGhost <resets. 31742263+rshnGhost@users.noreply.github.com>
2021-08-12 13:26:20 +0530

    testing: checking kb intrupt

--- installPython.ps1
+++ installPython.ps1
@@ -1,5 +1,13 @@
-Write-Host "Waiting"
-$key = [Console]::ReadKey($true)
-if ($key.key -eq "C" -and $key.modifiers -eq "Control") { 
-	Write-Host "Done"
+begin { 
+	[Console]::TreatControlCAsInput = $true
+}
+process {
+	Write-Host "Waiting"
+	$key = [Console]::ReadKey($true)
+	if ($key.key -eq "C" -and $key.modifiers -eq "Control") { 
+		Write-Host "Done"
+	}
+}
+end { 
+	[Console]::TreatControlCAsInput = $false
 }
