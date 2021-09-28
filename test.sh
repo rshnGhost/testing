87b9125db0cb1283601f98a8888421bc277ba96a
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-09-28 22:11:20 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,2 @@
-curl -s 'https://api.github.com/users/lambda' | fx .sha
-curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | fx .sha
-curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r .sha
+curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.sha'
 echo "rshn"
