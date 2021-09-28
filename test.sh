b6196e725d7f980bf4c8230574b487498ab1bd78
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-09-28 21:36:38 +0530

    tetsing 5

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 curl -s 'https://api.github.com/users/lambda' | fx .sha
 curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | fx .sha
+curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits'
