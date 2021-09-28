273ee36121c328e887bf573b1a471766652de66f
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-09-28 21:30:17 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-curl -sS https://webinstall.dev/jq | bash
+curl -sS 'https://webinstall.dev/jq' | bash
 curl -s 'https://api.github.com/users/lambda' | jq -r '.name'
 
 curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.sha'
