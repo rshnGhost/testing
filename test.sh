606835efc6215b6ce464fc69d55a745706d83953
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-09-28 21:27:03 +0530

    test 2

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 curl -s 'https://api.github.com/users/lambda' | jq -r '.name'
 
 curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.sha'
+
+curl -s 'http://twitter.com/users/username.json' | sed -e 's/[{}]/''/g' | awk -v RS=',"' -F: '/^text/ {print $2}'
