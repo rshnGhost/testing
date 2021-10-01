a450af54401486a2ac4c73d2854868dfe7e99123
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-10-01 15:21:25 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2;fi
 
 sha = curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.[0].sha' | cut -c1-7
-echo "rshn"
-echo sha
+echo "testing"
+echo $sha
