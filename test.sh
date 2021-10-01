ab5f8cda0475e7d1b29dcce43abad2febe39be01
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-10-01 15:11:16 +0530

    testing with package manager

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
-curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.sha'
-echo "rshn"
+packagesNeeded='curl jq'
+if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache $packagesNeeded
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install $packagesNeeded
+elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
+elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2;fi
+
+curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.[0].sha' | cut -c1-7
