c49e72dfafc6fe07bc1123a8052dd01bd8223cc9
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2021-10-01 15:17:21 +0530

    testing variable

--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ elif [ -x "$(command -v dnf)" ];     then sudo dnf install $packagesNeeded
 elif [ -x "$(command -v zypper)" ];  then sudo zypper install $packagesNeeded
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2;fi
 
-curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.[0].sha' | cut -c1-7
+sha = curl -s 'https://api.github.com/repos/rshnGhost/django-quick/commits' | jq -r '.[0].sha' | cut -c1-7
+echo sha
